9c72c19ba3e0e5ba8044a5e356e5275f92fa2664
hellrung <hellrung@83f8c8f9-255a-4e71-9441-f203ff132db1>
2010-04-19 17:58:18 +0000

    NEDUnitTests testProperties / testInitWithDynamicData

--- tests/NEDTests/NEDesignElementDynTest.m
+++ tests/NEDTests/NEDesignElementDynTest.m
@@ -7,35 +7,115 @@
 //
 
 #import "NEDesignElementDynTest.h"
-#import "NEDesignElementDyn.h"
+#import "../../src/NED/NEDesignElementDyn.h"
+#import "../../src/CLETUS/COSystemConfig.h"
 
 @interface NEDesignElementDynTest (MemberVariables)
+
 	BADesignElement *designEl;
+	COSystemConfig *config;
 @end
 
 @implementation NEDesignElementDynTest
 
 
 - (void) setUp {
-    designEl = [[NEDesignElementDyn alloc]
-                initWithFile:@"../tests/BARTMainAppTests/testfiles/erDesignTest01.des" 
-                ofImageDataType:IMAGE_DATA_FLOAT];
+	config = [COSystemConfig getInstance];
+	[config fillWithContentsOfEDLFile:@"/Users/Lydi/Development/BARTProcedure/BARTApplication/trunk/tests/NEDTests/timeBasedRegressorNEDTest.edl"];
+	
 }
 
 - (void) testProperties {
-   // STAssertEquals(designEl.mNumberTimesteps, 396, @"Incorrect number of timesteps.");
-//    STAssertEquals(designEl.mNumberExplanatoryVariables, 5, @"Incorrect number of covariates.");
-//    STAssertEquals(designEl.mRepetitionTimeInMs, 2000, @"Incorrect repetition time.");
-    STAssertEquals(designEl.mImageDataType, IMAGE_DATA_FLOAT, @"Incorrect image data type.");
+	[designEl release];
+	designEl = [[BADesignElement alloc] init];
+	
+	//STAssertEquals();
+	//STAssertTrue 
+	STAssertTrue([designEl mNumberTimesteps] == 0, @"initial value in design not null");
+	[designEl setMNumberTimesteps: 896];
+	STAssertTrue([designEl mNumberTimesteps] == 896, @"set positive value not correctly");
+	[designEl setMNumberTimesteps: -896];
+	
+	FILE* fp = fopen("/tmp/nedTest.txt", "w");
+	fputc('\n', fp);
+	fputc('B', fp);
+	fputc(' ', fp);
+	fprintf(fp, "test: %d", designEl.mNumberTimesteps);	 
+	fclose(fp);
+	
+	STAssertTrue([designEl mNumberTimesteps] == (-896), @"set value correct");
+	
+	
 }
 
 
 -(void) testInitWithDynamic {
-	//[designEl release];
-	//designEl = [[BADesignElementDyn alloc] initWithDynamicDataOfImageDataType:IMAGE_DATA_FLOAT];
+	
+	[config setProp:@"$nrTimesteps" :@"100"]; 
+	[config setProp:@"$TR" :@"1000"];
+	unsigned int numberEvents = 2;
+	
+    for (unsigned int i = 0; i < numberEvents; i++)
+	{
+		NSString *stringTrialsInReg = [NSString stringWithFormat:@"$gwDesign/timeBasedRegressor[%d]/tbrDesign/statEvent", i+1, i+1];
+		NSUInteger nrTrialsInRegr = 10;
+		
+		for (unsigned int k = 0; k < nrTrialsInRegr; k++)
+		{
+			NSString *stringTrialTime = [NSString stringWithFormat:@"$gwDesign/timeBasedRegressor[%d]/tbrDesign/statEvent[%d]/@time", i+1, k+1];
+			NSString *stringTrialDuration = [NSString stringWithFormat:@"$gwDesign/timeBasedRegressor[%d]/tbrDesign/statEvent[%d]/@duration",i+1, k+1];
+			NSString *stringTrialHeight = [NSString stringWithFormat:@"$gwDesign/timeBasedRegressor[%d]/tbrDesign/statEvent[%d]/@height",i+1, k+1];
+			[config setProp:stringTrialTime :[NSString stringWithFormat:@"%d",(i+1) * 1000]];
+			[config setProp:stringTrialHeight :@"1"];
+			[config setProp:stringTrialDuration :@"20000"];
+		}
+	}
+	
+	
+	designEl = [[BADesignElement alloc]
+                initWithDynamicDataOfImageDataType: IMAGE_DATA_FLOAT];
+	// TODO: remove output
+	FILE* fp = fopen("/tmp/nedTest.txt", "w");
+	fputc('\n', fp);
+	fputc('B', fp);
+	fputc(' ', fp);
+	fprintf(fp, "test: %d", designEl.mNumberRegressors);	 
+	fclose(fp);
+	// END output
+	
+	STAssertEquals(designEl.mNumberCovariates, (unsigned int)(0), @"Incorrect number of covariates.");
+	STAssertEquals(designEl.mNumberRegressors, (unsigned int)(3), @"Incorrect number of regressors.");
+	STAssertEquals(designEl.mNumberTimesteps,  (unsigned int)100, @"Incorrect number of timesteps.");
+	STAssertEquals(designEl.mNumberExplanatoryVariables,(unsigned int) (3), @"Incorrect number of explanatory variables.");
+    STAssertEquals(designEl.mRepetitionTimeInMs, (unsigned int) (1000), @"Incorrect repetition time.");
+    STAssertEquals(designEl.mImageDataType, IMAGE_DATA_FLOAT, @"Incorrect image data type.");
+	
+	
+}
+
+-(void) testCopy
+{
 
 }
 
-//-(void) test2{}
+-(void)testInitWithFile
+{
+
+}
+
+-(void)testGenerateDesign
+{
+}
+
+-(void)testWriteDesignFile
+{
+
+}
+
+-(void)testGetValues
+{
+
+}
+
 
 @end
