8cd7ca65aeef6a953eb7e636936c95cf194795ea
hellrung <hellrung@83f8c8f9-255a-4e71-9441-f203ff132db1>
2010-04-20 16:50:00 +0000

    created EDNAUnitTestSuite

---
+++ tests/EDDataElementTestVI.h
@@ -0,0 +1,16 @@
+//
+//  EDDataElementTestVI.h
+//  BARTApplication
+//
+//  Created by Lydia Hellrung on 4/20/10.
+//  Copyright 2010 MPI Cognitive and Human Brain Sciences Leipzig. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+
+@interface EDDataElementTestVI : SenTestCase {
+
+}
+
+@end
