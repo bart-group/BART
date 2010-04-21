ed84ce4ab1135781cc001a86f07d4990be7c66eb
hellrung <hellrung@83f8c8f9-255a-4e71-9441-f203ff132db1>
2010-04-21 08:44:51 +0000

    next try to check-in new NEDUnitTestSuite

---
+++ tests/NEDTests/NEDesignElementDynTest.h
@@ -0,0 +1,16 @@
+//
+//  BADesignElementDynTest.h
+//  BARTApplication
+//
+//  Created by Lydia Hellrung on 3/2/10.
+//  Copyright 2010 MPI Cognitive and Human Brain Sciences Leipzig. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+
+@interface NEDesignElementDynTest : SenTestCase {
+
+}
+
+@end
