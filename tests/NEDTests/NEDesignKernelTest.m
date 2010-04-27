db38748c98f7c614fcb2e99c3b397ebaac027e37
hellrung <hellrung@83f8c8f9-255a-4e71-9441-f203ff132db1>
2010-04-27 15:47:30 +0000

    completed tests

--- tests/NEDTests/NEDesignKernelTest.m
+++ tests/NEDTests/NEDesignKernelTest.m
@@ -25,30 +25,29 @@ NEDesignKernel *kernel;
 	STAssertNil(kernel, @"init GeneralGamma doesn't return nil");
 	
 	GloverParams *gloverParams;
-	STAssertNoThrow( kernel = [[NEDesignKernel alloc] initWithGloverParams:gloverParams andNumberSamples:100], @" initWithGlover throws exception");
+	STAssertNoThrow( kernel = [[NEDesignKernel alloc] initWithGloverParams:gloverParams andNumberSamples:[NSNumber numberWithUnsignedLong:100] andSamplingRate:[NSNumber numberWithUnsignedLong:0]], @" initWithGlover throws exception");
 	// glover params not initialized and not set yet
 	STAssertNil(kernel, @"init GloverGamma returns not nil");
 
 	//	//init gloverParams but not set
 	gloverParams = [[GloverParams alloc] init];
-	STAssertNoThrow( kernel = [[NEDesignKernel alloc] initWithGloverParams:gloverParams andNumberSamples:100], @" initWithGlover throws exception");
+	STAssertNoThrow( kernel = [[NEDesignKernel alloc] initWithGloverParams:gloverParams andNumberSamples:[NSNumber numberWithUnsignedLong:100] andSamplingRate:[NSNumber numberWithUnsignedLong:10]], @" initWithGlover throws exception");
 	STAssertNotNil(kernel, @"init GloverGamma returns not nil");
 	[kernel release];
 	[gloverParams release];
 
-	// init and set glover params
-	//TODO: ABH numberSamples!!!!!!
-	gloverParams = [[GloverParams alloc] initWithMaxLength:300 peak1:6.0 scale1:0.9 peak2:12.0 scale2:0.9 offset:0.0
-													  relationP1P2:0.1 heightScale:120];
-	STAssertNoThrow( kernel = [[NEDesignKernel alloc] initWithGloverParams:gloverParams andNumberSamples:15], @" initWithGlover throws exception");
+//	// init and set glover params
+	gloverParams = [[GloverParams alloc] initWithMaxLength:30 peak1:60 scale1:0.9 peak2:900 scale2:0.9 offset:0.0
+											  relationP1P2:0.1 heightScale:120 givenInTimeUnit:KERNEL_TIME_MS];
+	STAssertNoThrow( kernel = [[NEDesignKernel alloc] initWithGloverParams:gloverParams andNumberSamples:[NSNumber numberWithUnsignedLong:10] andSamplingRate:[NSNumber numberWithUnsignedLong:20]], @" initWithGlover throws exception");
 	STAssertNotNil(kernel, @"init GloverGamma returns nil");
 	STAssertTrue([kernel isKindOfClass:[NEDesignGloverKernel class]], @"initWithGloverParams doesn't return correct class type");
 	[kernel release];
 	[gloverParams release];
 	
 	gloverParams = [[GloverParams alloc] initWithMaxLength:0 peak1:0 scale1:0 peak2:0 scale2:0 offset:0.0
-											  relationP1P2:0 heightScale:0];
-	STAssertNoThrow( kernel = [[NEDesignKernel alloc] initWithGloverParams:gloverParams andNumberSamples:0], @" initWithGlover throws exception");
+											  relationP1P2:0 heightScale:0 givenInTimeUnit:KERNEL_TIME_MS];
+	STAssertNoThrow( kernel = kernel = [[NEDesignKernel alloc] initWithGloverParams:gloverParams andNumberSamples:[NSNumber numberWithUnsignedLong:15000] andSamplingRate:[NSNumber numberWithUnsignedLong:20]], @" initWithGlover throws exception");
 	
 }
 
