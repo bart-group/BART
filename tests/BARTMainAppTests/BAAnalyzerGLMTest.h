25158f68a221508da91ad37621aa20c8fb26c86d
hellrung <hellrung@83f8c8f9-255a-4e71-9441-f203ff132db1>
2010-08-30 14:50:08 +0000

    BAAnalyzerGLMTest added

---
+++ tests/BARTMainAppTests/BAAnalyzerGLMTest.h
@@ -0,0 +1,16 @@
+//
+//  BAAnalyzerGLMTest.h
+//  BARTApplication
+//
+//  Created by Lydia Hellrung on 8/30/10.
+//  Copyright 2010 MPI Cognitive and Human Brain Sciences Leipzig. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+
+@interface BAAnalyzerGLMTest : SenTestCase {
+
+}
+
+@end
---
+++ tests/BARTMainAppTests/BAAnalyzerGLMTest.m
@@ -0,0 +1,14 @@
+//
+//  BAAnalyzerGLMTest.m
+//  BARTApplication
+//
+//  Created by Lydia Hellrung on 8/30/10.
+//  Copyright 2010 MPI Cognitive and Human Brain Sciences Leipzig. All rights reserved.
+//
+
+#import "BAAnalyzerGLMTest.h"
+
+
+@implementation BAAnalyzerGLMTest
+
+@end
