337aa5c36fd3d1109c713608a76a43fac176ff3c
hellrung <hellrung@83f8c8f9-255a-4e71-9441-f203ff132db1>
2011-04-05 18:23:50 +0000

    missing header added

--- src/BARTMainApp/BARTNotifications.m
+++ src/BARTMainApp/BARTNotifications.m
@@ -5,8 +5,9 @@
 //  Created by Lydia Hellrung on 3/25/11.
 //  Copyright 2011 MPI Cognitive and Human Brain Sciences Leipzig. All rights reserved.
 //
+#import "BARTNotifications.h"
 
-NSString *const BARTDidLoadNextDataNotification = @"BARTNextDataNotification";
-NSString *const BARTDidCalcNextResultNotification = @"BARTNextResultNotification";
-NSString *const BARTDidLoadBackgroundImageNotification = @"BARTLoadedBackgroundImageFinished";
-NSString *const BARTDidReceiveNextTriggerNotification = @"BARTNextTriggerNotification";
+NSString * const BARTDidLoadNextDataNotification = @"BARTNextDataNotification";
+NSString * const BARTDidCalcNextResultNotification = @"BARTNextResultNotification";
+NSString * const BARTDidLoadBackgroundImageNotification = @"BARTLoadedBackgroundImageFinished";
+NSString * const BARTDidReceiveNextTriggerNotification = @"BARTNextTriggerNotification";
