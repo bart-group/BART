ee00fd2b3923f366b174f654d6a937b6aca7c295
lydiatgit <hellrung@cbs.mpg.de>
2011-07-28 19:14:12 +0200

    added TROY created BAFunctor as base class for functors

---
+++ projects/TROYTests/TROYTests.h
@@ -0,0 +1,17 @@
+//
+//  TROYTests.h
+//  TROYTests
+//
+//  Created by Lydia Hellrung on 7/28/11.
+//  Copyright 2011 MPI CBS. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+
+@interface TROYTests : SenTestCase {
+@private
+    
+}
+
+@end
---
+++ projects/TROYTests/TROYTests.m
@@ -0,0 +1,33 @@
+//
+//  TROYTests.m
+//  TROYTests
+//
+//  Created by Lydia Hellrung on 7/28/11.
+//  Copyright 2011 MPI CBS. All rights reserved.
+//
+
+#import "TROYTests.h"
+
+
+@implementation TROYTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in TROYTests");
+}
+
+@end
