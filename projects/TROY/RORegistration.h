2dc8860c4fedea974054a1e468e64bc4868c7aa4
Oliver Zscheyge <oliver@192-168-128-56.cbs.mpg.de>
2011-10-12 16:58:15 +0200

    Integrated valign/dotrans3d into RORegistration (compiles). Still not file independent (itkImageFileReaderException).

--- projects/TROY/RORegistration.h
+++ projects/TROY/RORegistration.h
@@ -10,15 +10,49 @@
 
 #import "EDDataElement.h"
 
-#include "isisRegistrationFactory3D.hpp"
+// ISIS includes
+#import "isisRegistrationFactory3D.hpp"
+#import "isisTimeStepExtractionFilter.hpp"
 
+// ITK includes
+#import <itkImageFileReader.h>
+#import <itkWarpImageFilter.h>
+#import <itkTileImageFilter.h>
+
+
+// ISIS typedef
 typedef isis::registration::RegistrationFactory3D<ITKImage, ITKImage> RegistrationFactoryType;
+typedef isis::extitk::TimeStepExtractionFilter<ITKImage4D, ITKImage> TimeStepExtractionFilterType;
 
+// ITK typedefs
+typedef itk::Vector<float, 3> VectorType;
+typedef itk::Image<VectorType, 3> DeformationFieldType;
+typedef itk::ImageFileReader<DeformationFieldType> DeformationFieldReaderType;
+
+typedef itk::ResampleImageFilter<ITKImage, ITKImage> ResampleImageFilterType;
+typedef itk::WarpImageFilter<ITKImage, ITKImage, DeformationFieldType> WarpImageFilterType;
+
+typedef itk::LinearInterpolateImageFunction<ITKImage, double> LinearInterpolatorType;
+typedef itk::TileImageFilter<ITKImage, ITKImage4D> TileImageFilterType;
+
+
+
+
+/**
+ * Based on valign3d and vdotrans from Lipsia.
+ */
 @interface RORegistration : NSObject {
     
     @private
     RegistrationFactoryType::Pointer registrationFactory;
+    /** Holds the transformation information gathered during init. */
+    const itk::TransformBase* tmpConstTransformPointer;
     
+    /** Applies the transformation to an ISIS image. */
+    ResampleImageFilterType::Pointer resampler;
+    WarpImageFilterType::Pointer warper;
+    
+    LinearInterpolatorType::Pointer linearInterpolator;
 }
 
 /*
@@ -29,6 +63,7 @@ typedef isis::registration::RegistrationFactory3D<ITKImage, ITKImage> Registrati
  *    and returns the transformed DataElement
  */
 //-(RORegistration*)initWithParams:
+-(id)initFindingTransformFrom:(EDDataElement*)toAlign toReference:(EDDataElement*)ref;
 
 /**
  *
--- projects/TROY/RORegistration.mm
+++ projects/TROY/RORegistration.mm
@@ -10,34 +10,94 @@
 
 #import "EDDataElement.h"
 
+// Isis
+#import "DataStorage/image.hpp"
+#import "Adapter/itkAdapter.hpp"
+
+const int NR_THREADS = 4;
+
+@interface RORegistration (privateMethods)
+
+-(const itk::TransformBase*)computeTransform:(EDDataElement*)toAlign 
+                               withReference:(EDDataElement*)ref;
+
+-(EDDataElement*)transform:(EDDataElement*)toAlign 
+             withReference:(EDDataElement*)ref 
+            transformation:(const itk::TransformBase*)trans 
+                resolution:(std::vector<size_t>)res 
+            functionalData:(BOOL)fmri; 
+
+@end
+
 @implementation RORegistration
 
 -(id)init
 {
     if (self = [super init]) {
+        // From align3d
         self->registrationFactory = RegistrationFactoryType::New();
+        
+        // From dotrans3d
+        self->resampler = ResampleImageFilterType::New();
+        self->warper = WarpImageFilterType::New();
+        self->linearInterpolator = LinearInterpolatorType::New();
+
     }
     
     return self;    
 }
 
-bool verbose = true;
+
+-(id)initFindingTransformFrom:(EDDataElement*)toAlign 
+                  toReference:(EDDataElement*)ref
+{
+    if (self = [self init]) {
+        self->tmpConstTransformPointer = [self computeTransform:toAlign withReference:ref];
+    }
+    
+    return self;
+}
 
 -(EDDataElement*)align:(EDDataElement*)toAlign withReference:(EDDataElement*)ref 
 {
     if (toAlign == nil || ref == nil) {
-        // TODO: throw exception?
+        // exception?
         return nil;
     }
     
+    // TODO: replace with method: align :: EDDataElement -> EDDataElement
+    
+    const itk::TransformBase* trans = [self computeTransform:toAlign withReference:ref];
+    
+    std::vector<size_t> res;
+    res.push_back(1);
+    
+    EDDataElement* transformed = [self transform:toAlign 
+                                   withReference:ref 
+                                  transformation:trans 
+                                      resolution:res 
+                                  functionalData:NO];
+    
+    return transformed;
+}
+
+@end
+
+@implementation RORegistration (privateMethods)
+
+bool verbose = true;
+
+-(const itk::TransformBase*)computeTransform:(EDDataElement*)toAlign 
+withReference:(EDDataElement*)ref
+{
     self->registrationFactory->Reset();
     
     self->registrationFactory->SetTransform(RegistrationFactoryType::VersorRigid3DTransform);
-//    self->registrationFactory->SetTransform(RegistrationFactoryType::AffineTransform); 
-
+    //    self->registrationFactory->SetTransform(RegistrationFactoryType::AffineTransform); 
+    
     self->registrationFactory->SetMetric(RegistrationFactoryType::MeanSquareMetric);
-//    self->registrationFactory->SetMetric(RegistrationFactoryType::NormalizedCorrelationMetric); // very runtime inefficient
-//    self->registrationFactory->SetMetric(RegistrationFactoryType::MutualInformationHistogramMetric); // even more runtime inefficient
+    //    self->registrationFactory->SetMetric(RegistrationFactoryType::NormalizedCorrelationMetric); // very runtime inefficient
+    //    self->registrationFactory->SetMetric(RegistrationFactoryType::MutualInformationHistogramMetric); // even more runtime inefficient
     self->registrationFactory->SetInterpolator(RegistrationFactoryType::LinearInterpolator);
     self->registrationFactory->SetOptimizer(RegistrationFactoryType::RegularStepGradientDescentOptimizer);    
     
@@ -55,7 +115,7 @@ bool verbose = true;
     self->registrationFactory->UserOptions.PREALIGNPRECISION = 5;
     
     // TODO: remove
-    if ( verbose ) {
+    if (verbose) {
         registrationFactory->UserOptions.SHOWITERATIONATSTEP = 1;
         registrationFactory->UserOptions.PRINTRESULTS = true;
     } else {
@@ -63,10 +123,10 @@ bool verbose = true;
         registrationFactory->UserOptions.PRINTRESULTS = false;
     }
     // remove END
-        
+    
     self->registrationFactory->UserOptions.NumberOfThreads = 1;
     self->registrationFactory->UserOptions.MattesMutualInitializeSeed = 1;
-
+    
     // Convert data elements to ITK images.
     ITKImage::Pointer refITKImg = [ref asITKImage];
     ITKImage::Pointer toAlignITKImg = [toAlign asITKImage];
@@ -76,9 +136,207 @@ bool verbose = true;
     // Causes unit tests to crash if the MattesMutualInformationMetric is used
     self->registrationFactory->StartRegistration();
     
-    const itk::TransformBase* tmpConstTransformPointer;
-    tmpConstTransformPointer = registrationFactory->GetTransform();
+    return registrationFactory->GetTransform();
+}
+
+-(EDDataElement*)transform:(EDDataElement*)toAlign 
+             withReference:(EDDataElement*)ref 
+            transformation:(const itk::TransformBase*)trans 
+                resolution:(std::vector<size_t>)res
+            functionalData:(BOOL)fmri
+{
+    self->resampler->SetNumberOfThreads(NR_THREADS);
+    self->warper->SetNumberOfThreads(NR_THREADS);
+
+    ITKImage::Pointer inputImage = ITKImage::New();
+    ITKImage::Pointer templateImage = ITKImage::New();
+    ITKImage4D::Pointer fmriImage = ITKImage4D::New();
+
+    templateImage = [ref asITKImage];
     
+    ITKImage::SizeType    outputSize;
+    ITKImage::SpacingType outputSpacing;
+    ITKImage::SizeType fmriOutputSize;
+	ITKImage::SpacingType fmriOutputSpacing;
+    
+    //setting up the output resolution
+	if (res.size() > 0) {
+		if (static_cast<unsigned int> (res.size()) < IMAGE_DIMENSION) {
+			//user has specified less than 3 resolution values->sets isotrop resolution with the first typed value
+			outputSpacing.Fill(res[0]);
+		}
+        
+		if (res.size() >= 3 ) {
+			//user has specified at least 3 values -> sets anisotrop resolution
+			for ( unsigned int i = 0; i < 3; i++ ) {
+				outputSpacing[i] = res[i];
+			}
+		}
+	} else {
+		if (ref == nil) {
+			outputSpacing = inputImage->GetSpacing();
+			outputSize = inputImage->GetLargestPossibleRegion().GetSize();
+		} else {
+            outputSpacing = templateImage->GetSpacing();
+			outputSize = templateImage->GetLargestPossibleRegion().GetSize();
+        }
+	}
+
+    
+	if (fmri) {
+		fmriImage  = [toAlign asITKImage4D];
+	} else {
+		inputImage = [toAlign asITKImage];
+    }
+    
+    ITKImage::PointType outputOrigin;
+	ITKImage::DirectionType outputDirection;
+    
+    if (ref == nil) {
+		outputDirection = inputImage->GetDirection();
+		outputOrigin = inputImage->GetOrigin();
+	} else {
+		outputDirection = templateImage->GetDirection();
+		outputOrigin = templateImage->GetOrigin();
+	}
+
+
+    
+    // TODO: skipped setting the output direction/origin setting
+    
+    // TODO: skipped setting up the deformation field
+    
+    // TODO: skipped computing the output size
+    
+    DeformationFieldReaderType::Pointer deformationFieldReader = DeformationFieldReaderType::New();
+    DeformationFieldType::Pointer defField = DeformationFieldType::New();
+	defField = deformationFieldReader->GetOutput();    
+    
+    //setting up the interpolator
+    // TODO: make configurable
+    self->resampler->SetInterpolator( linearInterpolator );
+    self->warper->SetInterpolator( linearInterpolator );
+//    self->resampler->SetInterpolator( bsplineInterpolator );
+//    self->warper->SetInterpolator( bsplineInterpolator );
+//    self->resampler->SetInterpolator( nearestNeighborInterpolator );
+//    self->warper->SetInterpolator( nearestNeighborInterpolator );
+    
+    ITKImage::PointType fmriOutputOrigin;
+    ITKImage::DirectionType fmriOutputDirection;
+    
+    isis::adapter::itkAdapter* adapter = new isis::adapter::itkAdapter;
+    
+	if (fmri) {
+        TimeStepExtractionFilterType::Pointer timeStepExtractionFilter = TimeStepExtractionFilterType::New();
+		timeStepExtractionFilter->SetInput(fmriImage);
+        
+//		if (template_filename) {
+			fmriOutputOrigin = templateImage->GetOrigin();
+			fmriOutputDirection = templateImage->GetDirection();
+//		}
+        
+		for ( unsigned int i = 0; i < 4; i++ ) {
+			if (res.size()) {
+				fmriOutputSpacing[i] = outputSpacing[i];
+				fmriOutputSize[i] = outputSize[i];
+			} else {
+				if (ref == nil) {
+					fmriOutputSpacing[i] = fmriImage->GetSpacing()[i];
+					fmriOutputSize[i] = fmriImage->GetLargestPossibleRegion().GetSize()[i];
+				} else {
+					fmriOutputSpacing[i] = templateImage->GetSpacing()[i];
+					fmriOutputSize[i] = templateImage->GetLargestPossibleRegion().GetSize()[i];
+				}
+			}
+            
+			if (ref == nil) {
+				fmriOutputOrigin[i] = fmriImage->GetOrigin()[i];
+                
+				for ( unsigned int j = 0; j < 3; j++ ) {
+					fmriOutputDirection[j][i] = fmriImage->GetDirection()[j][i];
+				}
+			}
+		}
+        
+		if (fmriOutputSpacing[3] == 0) 
+            fmriOutputSpacing[3] = 1; 
+        
+		if (trans != NULL) {
+			self->warper->SetOutputDirection(fmriOutputDirection);
+			self->warper->SetOutputOrigin(fmriOutputOrigin );
+			self->warper->SetOutputSize(fmriOutputSize);
+			self->warper->SetOutputSpacing(fmriOutputSpacing);
+			self->warper->SetInput(inputImage);
+            self->warper->SetDeformationField(defField);
+		}
+        
+		itk::FixedArray<unsigned int, 4> layout;
+		layout[0] = 1;
+		layout[1] = 1;
+		layout[2] = 1;
+		layout[3] = 0;
+		const unsigned int numberOfTimeSteps = fmriImage->GetLargestPossibleRegion().GetSize()[3];
+		ITKImage::Pointer tileImage;
+		std::cout << std::endl;
+		inputImage = [toAlign asITKImage];
+		ITKImage4D::DirectionType direction4D;
+        
+		for ( size_t i = 0; i < 3; i++ ) {
+			for ( size_t j = 0; j < 3; j++ ) {
+				direction4D[i][j] = fmriOutputDirection[i][j];
+			}
+		}
+        
+		direction4D[3][3] = 1;
+        
+        ITKImage::Pointer tmpImage = ITKImage::New();
+        TileImageFilterType::Pointer tileImageFilter = TileImageFilterType::New();
+        
+		for ( unsigned int timestep = 0; timestep < numberOfTimeSteps; timestep++ ) {
+			std::cout << "Resampling timestep: " << timestep << "...\r" << std::flush;
+			timeStepExtractionFilter->SetRequestedTimeStep( timestep );
+			timeStepExtractionFilter->Update();
+			tmpImage = timeStepExtractionFilter->GetOutput();
+			tmpImage->SetDirection( inputImage->GetDirection() );
+			tmpImage->SetOrigin( inputImage->GetOrigin() );
+            
+			if (trans != NULL) {
+				warper->SetInput( tmpImage );
+				warper->Update();
+				tileImage = warper->GetOutput();
+			}
+            
+			tileImage->Update();
+			tileImage->DisconnectPipeline();
+			tileImageFilter->PushBackInput( tileImage );
+		}
+        
+		tileImageFilter->SetLayout( layout );
+		tileImageFilter->GetOutput()->SetDirection( direction4D );
+		tileImageFilter->Update();
+		std::list<isis::data::Image> imgList = adapter->makeIsisImageObject<ITKImage4D>(tileImageFilter->GetOutput());
+        
+//		isis::data::IOFactory::write( imgList, out_filename, "" , "" );
+	} else {
+        // No fmri
+//        writer->SetFileName( out_filename );
+        
+//		if ( vtrans_filename or trans_filename.number > 1 && !identity ) {
+        if (trans != NULL) {
+			self->warper->SetOutputDirection(outputDirection);
+			self->warper->SetOutputOrigin(outputOrigin);
+			self->warper->SetOutputSize(outputSize);
+			self->warper->SetOutputSpacing(outputSpacing);
+			self->warper->SetInput(inputImage);
+            
+            self->warper->SetDeformationField(defField);
+			            
+			self->warper->Update();
+			std::list<isis::data::Image> imgList = adapter->makeIsisImageObject<ITKImage>( warper->GetOutput() );
+//			isis::data::IOFactory::write( imgList, out_filename, "", "" );
+		}
+    }
+
     return nil;
 }
 
