02327d3b839d4fdc3e6be4808ba7c64802e41801
Oliver Zscheyge <oliver@192-168-128-56.cbs.mpg.de>
2011-12-02 18:45:53 +0100

    Add adapter conversion test

--- tests/EDNATests/isis_test.mm
+++ tests/EDNATests/isis_test.mm
@@ -53,7 +53,8 @@ void testVnormdataRegistrationWorkflow() {
                                        beingFunctional:NO                   // YES
                                          withReference:mniData];
     
-    [ana2fct2mni WriteDataElementToFile:@"/tmp/IsisTestDataElem.nii"];
+    [ana2fct WriteDataElementToFile:@"/tmp/IsisTestAna2Fct.nii"];
+    [ana2fct2mni WriteDataElementToFile:@"/tmp/IsisTestAna2Fct2Mni.nii"];
     
     // Cleanup
     [mniData release];
@@ -65,7 +66,45 @@ void testVnormdataRegistrationWorkflow() {
 	[pool drain];
 }
 
+void testAdapterConversion() {
+    NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
+    
+    // Functional data
+    NSString* fctFile = @"/Users/oliver/test/reg3d_test/dataset01/data.nii";
+    EDDataElementIsis* fctData = [[EDDataElementIsis alloc] initWithFile:fctFile
+                                                               andSuffix:@"" 
+                                                              andDialect:@"" 
+                                                             ofImageType:IMAGE_FCTDATA];
+    
+    ITKImage4D::Pointer fctITK = [fctData asITKImage4D];
+    EDDataElement* fctDataConverted = [fctData convertFromITKImage4D:fctITK];
+    
+    [fctData WriteDataElementToFile:@"/tmp/adapter_fct.nii"];
+    [fctDataConverted WriteDataElementToFile:@"/tmp/adapter_fct_converted.nii"];
+    
+    [fctData release];
+    
+    
+    // Anatomical data
+    NSString* anaFile = @"/Users/oliver/test/reg3d_test/dataset01/ana.nii"; //_visotrop.nii";
+    EDDataElementIsis* anaData = [[EDDataElementIsis alloc] initWithFile:anaFile
+                                                               andSuffix:@"" 
+                                                              andDialect:@"" 
+                                                             ofImageType:IMAGE_ANADATA];
+    
+    ITKImage::Pointer anaITK = [anaData asITKImage];
+    EDDataElement* anaDataConverted = [anaData convertFromITKImage:anaITK];
+    
+    [anaData WriteDataElementToFile:@"/tmp/adapter_ana.nii"];
+    [anaDataConverted WriteDataElementToFile:@"/tmp/adapter_ana_converted.nii"];
+    
+    [anaData release];
+    
+	[pool drain];
+}
+
 int main(void)
 {
     testVnormdataRegistrationWorkflow();
+//    testAdapterConversion();
 }
