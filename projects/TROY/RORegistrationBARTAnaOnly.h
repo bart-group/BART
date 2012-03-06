5c34f30c770da5680e0e457088c056f27df058d6
ooz <oliverzscheyge@gmail.com>
2012-03-06 11:00:47 +0100

    Add BART registration method with fun2ana only

---
+++ projects/TROY/RORegistrationBARTAnaOnly.h
@@ -0,0 +1,13 @@
+//
+//  RORegistrationBARTAnaOnly.h
+//  BARTApplication
+//
+//  Created by Oliver Zscheyge on 3/6/12.
+//  Copyright (c) 2012 MPI Cognitive and Human Brain Sciences Leipzig. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface RORegistrationBARTAnaOnly : NSObject
+
+@end
---
+++ projects/TROY/RORegistrationBARTAnaOnly.mm
@@ -0,0 +1,13 @@
+//
+//  RORegistrationBARTAnaOnly.m
+//  BARTApplication
+//
+//  Created by Oliver Zscheyge on 3/6/12.
+//  Copyright (c) 2012 MPI Cognitive and Human Brain Sciences Leipzig. All rights reserved.
+//
+
+#import "RORegistrationBARTAnaOnly.h"
+
+@implementation RORegistrationBARTAnaOnly
+
+@end
