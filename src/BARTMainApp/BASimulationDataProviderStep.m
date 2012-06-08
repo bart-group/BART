81e58fc70f934f97e0aeafd8bf25d936bbe6c8ef
Torsten Schlumm <torsten@zuse.cbs.mpg.de>
2012-06-08 15:58:28 +0200

    reading tree structure from file and some more on steps

--- src/BARTMainApp/BASimulationDataProviderStep.m
+++ src/BARTMainApp/BASimulationDataProviderStep.m
@@ -1,20 +1,20 @@
 //
-//  BAExampleStep.m
+//  BASimulationDataProviderStep.m
 //  BARTApplication
 //
-//  Created by Torsten Schlumm on 6/5/12.
+//  Created by Torsten Schlumm on 6/8/12.
 //  Copyright (c) 2012 MPI Cognitive and Human Brain Sciences Leipzig. All rights reserved.
 //
 
-#import "BAExampleStep.h"
+#import "BASimulationDataProviderStep.h"
 
-@implementation BAExampleStep
+@implementation BASimulationDataProviderStep
 
 - (id)initWithNameAndComment:(NSString *)name comment:(NSString *)comment
 {
     if(self = [super initWithNameAndComment:name comment:comment])
     {
-        [[self properties] setObject:@"BAExampleStepConfigView" forKey: BA_ELEMENT_PROPERTY_CONFIGURATION_UI_NAME];
+        [[self properties] setObject:@"BASimulationDataProviderConfigView" forKey: BA_ELEMENT_PROPERTY_CONFIGURATION_UI_NAME];
     }
     
     return self;
