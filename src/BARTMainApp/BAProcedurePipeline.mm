b3aa1a6072fac59503486a5f4f022cdacd93bde9
lydiatgit <hellrung@cbs.mpg.de>
2012-09-06 13:50:49 +0200

    just stuff for simulation

--- src/BARTMainApp/BAProcedurePipeline.mm
+++ src/BARTMainApp/BAProcedurePipeline.mm
@@ -54,7 +54,7 @@
 {
     if ((self = [super init])) {
         // TODO: appropriate init
-        mCurrentTimestep = 1;
+        mCurrentTimestep = 0;
 		config = [[COExperimentContext getInstance] systemConfig];
 		isRealTimeTCPInput = NO;
 		startAnalysisAtTimeStep = 15;
@@ -240,8 +240,8 @@
     memset(cVecFromConfig, 0, (sizeof(float)* [[paradigm designElement] numberExplanatoryVariables] ));
 	cVecFromConfig[0] = 1.0;
 	cVecFromConfig[1] = 1.0;
-    cVecFromConfig[0] = -1.0;
-	cVecFromConfig[1] = -1.0;
+    cVecFromConfig[2] = -1.0;
+    cVecFromConfig[3] = -1.0;
 	//cVecFromConfig[2] = 0.0;
 	NSMutableArray *contrastVector = [[NSMutableArray alloc] init];
 	for (size_t i = 0; i < [[paradigm designElement] numberExplanatoryVariables]; i++){
