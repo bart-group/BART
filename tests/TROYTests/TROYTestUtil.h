b3a2a3c83eee58c82fdf4104255fce50326ff996
ooz <oliverzscheyge@gmail.com>
2013-02-12 16:15:51 +0100

    Add new util methods for measuring registration runtime

--- tests/TROYTests/TROYTestUtil.h
+++ tests/TROYTests/TROYTestUtil.h
@@ -9,6 +9,7 @@
 #import <Foundation/Foundation.h>
 
 @class EDDataElement;
+@class RORegistrationMethod;
 
 /**
  * Utilities for testing the BART registration module.
@@ -28,6 +29,35 @@
 -(void)redirect:(FILE*)stream
              to:(NSString*)filepath
           using:(NSString*)mode;
+
+/**
+ * Measures the runtime of a RORegistrationMethod for both finding and applying the
+ * transformation.
+ * This method prints the result using NSLog and also returns a tuple containing both
+ * the time needed to find the transformation as well as the time to apply the
+ * transformation.
+ *
+ * \param funPath Filepath for the functional image.
+ * \param anaPath Filepath for the anatomical image.
+ * \param mniPath Filepath to the MNI (template) file.
+ * \param outPath File to write the registration result to. 
+ *                Pass nil or empty NSString if you don't want to write the result.
+ * \param method  RORegistrationMethod that should be computed and applied.
+ *                Only pass the allocated class object! Do not initialize the object!
+ *                Initialization (= finding the transformation) happens in this method.
+ *                The caller is still responsible for releasing the object.
+ * \param runs    Number of iterations. The resulting runtime is averaged.
+ * \return        Auto-released NSArray of NSNumbers.
+ *                It is always a tuple where the first entry denotes the time to find
+ *                the registration transformation and the second entry tells the time 
+ *                to apply the transformation.
+ */
+-(NSArray*)measureRegistrationRuntime:(NSString*)funPath
+                              anatomy:(NSString*)anaPath
+                                  mni:(NSString*)mniPath
+                                  out:(NSString*)outPath
+                         registration:(RORegistrationMethod*)method
+                                 runs:(int)runs;
 
 /**
  * Creates the voxel-by-voxel difference of two EDDataElements.
--- tests/TROYTests/TROYTestUtil.mm
+++ tests/TROYTests/TROYTestUtil.mm
@@ -8,7 +8,11 @@
 
 #import "TROYTestUtil.h"
 
-#import "EDDataElement.h"
+#import "EDDataElementIsis.h"
+
+#import "RORegistrationMethod.h"
+
+#import "TimeUtil.h"
 
 @implementation TROYTestUtil
 
@@ -25,6 +29,85 @@
           using:(NSString*)mode
 {
     freopen([filepath UTF8String], [mode UTF8String], stream);
+}
+
+-(NSArray*)measureRegistrationRuntime:(NSString*)funPath
+                              anatomy:(NSString*)anaPath
+                                  mni:(NSString*)mniPath
+                                  out:(NSString*)outPath
+                         registration:(RORegistrationMethod*)method
+                                 runs:(int)runs
+{
+    TimeVal aliStart;
+    TimeVal aliEnd;
+    TimeVal appEnd;
+    TimeDiff* diff;
+    double alignTime = 0.0;
+    double applyTime = 0.0;
+    
+    for (int i = 0; i < runs; i++) {
+        
+        NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
+        
+        EDDataElementIsis* fctData = [[EDDataElementIsis alloc] initWithFile:funPath
+                                                                   andSuffix:@""
+                                                                  andDialect:@""
+                                                                 ofImageType:IMAGE_FCTDATA];
+        
+        EDDataElementIsis* anaData = [[EDDataElementIsis alloc] initWithFile:anaPath
+                                                                   andSuffix:@""
+                                                                  andDialect:@""
+                                                                 ofImageType:IMAGE_ANADATA];
+        
+        EDDataElementIsis* mniData = nil;
+        if ([mniPath length] > 0) {
+            mniData = [[EDDataElementIsis alloc] initWithFile:mniPath
+                                                    andSuffix:@""
+                                                   andDialect:@""
+                                                  ofImageType:IMAGE_ANADATA];
+        }
+        
+        aliStart = now(); // RUNTIME ANALYSIS CODE #
+        
+        method = [method initFindingTransform:fctData
+                                      anatomy:anaData
+                                    reference:mniData];
+        
+        aliEnd = now();   // RUNTIME ANALYSIS CODE #
+        
+        EDDataElement* result = [method apply:fctData];
+        
+        appEnd = now();   // RUNTIME ANALYSIS CODE #
+        diff = newTimeDiff(&aliStart, &aliEnd); // #
+        alignTime += asDouble(diff);            // #
+        free(diff);                             // #
+        diff = newTimeDiff(&aliEnd, &appEnd);   // #
+        applyTime += asDouble(diff);            // #
+        free(diff);       // RUNTIME ANALYSIS CODE #
+        
+        if ([outPath length] > 0) {
+            [result WriteDataElementToFile:outPath];
+        }
+        
+        if (mniData != nil) {
+            [mniData release];
+        }
+        
+        [anaData release];
+        [fctData release];
+        
+        [pool drain];
+    }
+    
+    if (runs > 0) {
+        alignTime /= static_cast<double>(runs);
+        applyTime /= static_cast<double>(runs);
+    }
+    
+    NSLog(@"Runtime for %d runs. Method: %@ fun: %@ ana: %@ mni: %@ out: %@. Registration: %lf s, application: %lf s",
+          runs, NSStringFromClass([method class]), funPath, anaPath, mniPath, outPath, alignTime, applyTime);
+    
+    return [NSArray arrayWithObjects:[NSNumber numberWithFloat:alignTime], [NSNumber numberWithFloat:applyTime], nil];
 }
 
 -(EDDataElement*)createDiffImage:(EDDataElement*)a
